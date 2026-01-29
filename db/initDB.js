136f45e1437493f982db7944381a92325386274d
Emrah Isovic <emrahisovic20@gmail.com>
2026-01-30 00:30:52 +0100

    updated initDB file

--- db/initDB.js
+++ db/initDB.js
@@ -6,6 +6,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 const INIT_SQL = `
